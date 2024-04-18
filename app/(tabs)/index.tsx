6171ecc8df7eaab3b05232fce036f39b58a5006b
Antony Jaison <antonyjaison639@gmail.com>
2024-04-18 12:36:14 +0530

    timer ui completed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,33 @@ import { View } from "@/components/Themed";
 import Footer from "@/components/footer";
 import MedicationIntakeStatus from "@/components/medication-intake-status";
 import Timer from "@/components/timer";
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 export default function TabOneScreen() {
+
+  const timer_gradient = [
+    { color: "#6d9cf4", percentage: 50 },
+    { color: "#1A4CD3", percentage: 100 },
+  ]
 
   return (
     <ScrollView>
       <View style={{ gap: 34 }} className="h-full bg-[#16161A] w-full px-4 flex-col py-4">
-        <Timer percentage={35} circleWidth={200}/>
+        <View className="bg-transparent w-full items-center">
+          <Timer
+            radius={80}
+            percentage={68}
+            strokeWidth={16}
+            circleWidth={200}
+            gradient={timer_gradient}
+            backgroundStrokeColor="none"
+          />
+          <Text className="text-white text-lg font-medium mt-4">Time for Next Medication</Text>
+        </View>
         <MedicationIntakeStatus />
         <DocumentsSection />
         <InsightSection />
-        <Footer/>
+        <Footer />
       </View>
     </ScrollView>
   );
--- components/timer.tsx
+++ components/timer.tsx
@@ -1,57 +1,53 @@
-import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Circle, Svg, Defs, LinearGradient, Stop } from 'react-native-svg'
 
-type TimerProps = {
-  percentage: number,
-  circleWidth: number
+type GradientType = {
+  color: string,
+  percentage: number
 }
 
-const Timer = ({ circleWidth, percentage }: TimerProps) => {
-  const radius = 85;
+type TimerProps = {
+  percentage: number,
+  circleWidth: number,
+  radius: number,
+  strokeWidth: number,
+  backgroundStrokeColor:string,
+  gradient: GradientType[]
+}
+
+const Timer = ({ circleWidth, percentage, gradient, radius, strokeWidth, backgroundStrokeColor }: TimerProps) => {
   const dashArray = radius * Math.PI * 2;
   const dashOffset = dashArray - (dashArray * percentage) / 100
   return (
-    <View>
-      <Svg width={circleWidth} height={circleWidth} viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
-        <Defs>
-          <LinearGradient id="gradient">
-            <Stop offset="10%" stopColor="#12c2e9" />
-            <Stop offset="50%" stopColor="#c471ed" />
-            <Stop offset="100%" stopColor="#f64f59" />
-          </LinearGradient>
-        </Defs>
-        <Circle
-          fill="none"
-          stroke="#ddd"
-          cx={circleWidth / 2}
-          cy={circleWidth / 2}
-          strokeWidth={15}
-          r={radius}
-        />
-        <Circle
-          cx={circleWidth / 2}
-          cy={circleWidth / 2}
-          strokeWidth={15}
-          r={radius}
-          strokeDasharray={dashArray}
-          strokeDashoffset={dashOffset}
-          fill="none"
-          stroke="url(#gradient)"
-          transform={`rotate(-90 ${circleWidth / 2} ${circleWidth / 2})`}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-
-        />
-      </Svg>
-    </View>
+    <Svg width={circleWidth} height={circleWidth} viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
+      <Defs>
+        <LinearGradient id="gradient">
+          {gradient.map((data, index) => <Stop key={index} offset={`${data.percentage}%`} stopColor={data.color} />)}
+        </LinearGradient>
+      </Defs>
+      <Circle
+        fill="none"
+        stroke={backgroundStrokeColor}
+        cx={circleWidth / 2}
+        cy={circleWidth / 2}
+        strokeWidth={strokeWidth}
+        r={radius}
+      />
+      <Circle
+        cx={circleWidth / 2}
+        cy={circleWidth / 2}
+        strokeWidth={strokeWidth}
+        r={radius}
+        strokeDasharray={dashArray}
+        strokeDashoffset={dashOffset}
+        fill="none"
+        stroke="url(#gradient)"
+        transform={`rotate(-90 ${circleWidth / 2} ${circleWidth / 2})`}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
   );
 };
-
-const styles = StyleSheet.create({
-  circleBackground: {
-
-  }
-});
 
 export default Timer;
